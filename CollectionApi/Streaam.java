1159025d322bf207a4af17183a1e1cbeb6dd677f
Muhammad-Aarij <aarijm5@gmail.com>
2024-07-17 23:09:33 +0500

    new

--- CollectionApi/Streaam.java
+++ CollectionApi/Streaam.java
@@ -9,12 +9,16 @@ public class Streaam {
 
         List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 6);
         Stream<Integer> s=num.stream();
-        Stream<Integer> s1=s.filter(n->n%2==0);
-        Stream<Integer> s2=s1.map(n->n*10);
-        
+        Stream<Integer> s1=s.filter(n->n>4);
+        Stream<Integer> s2=s1.map(n->n*n);
         
         s2.forEach(n->System.out.println(n));
         
+        int result =num.stream()
+                            .filter(n->n>4)
+                            .map(n->n*n)
+                            .reduce(0, (c,e)->c+e);        
+        System.out.println("result"+result);
     }
 
 }
